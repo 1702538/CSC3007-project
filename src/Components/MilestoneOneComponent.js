9fe8fbf0049aead6bc499cd3ea751cd26b7588fa
Keith Chong <70758815+1702538@users.noreply.github.com>
2022-06-23 17:15:34 +0800

    Added new code

--- src/Components/MilestoneOneComponent.js
+++ src/Components/MilestoneOneComponent.js
@@ -1,5 +1,13 @@
 function MilestoneOne() {
-  return <h1>123</h1>;
+  // JS code here
+  var a = "1234";
+
+  return (
+    <div>
+      {/* HTML code here */}
+      <h1>{a}</h1>
+    </div>
+  );
 }
 
 export default MilestoneOne;
